82f0e9208bf546df706acbab5a9666cbd4d4b1d7
sagarkaushal <engineersagarkaushal@gmail.com>
2021-05-05 18:07:56 +0530

    firstcommit

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+    required_version = "~> 0.14"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+
+    backend "s3" {
+
+        bucket = "kiaasa-bucket-infra"
+        key = "development/terraform.tfstate"
+        region = "ap-south-1"
+        dynamodb_table = "kiaasainfradevelopment"
+    }
+}
+
+provider "aws" {
+    region = var.aws_region
+    profile = "default"
+
+}
+
+
+
+
+#kiaasa-bucket-infra
+#kiaasainfradevelopment
