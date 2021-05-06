2f3098445638ec5d92ab5753a509cfc9613e15e5
sagarkaushal <engineersagarkaushal@gmail.com>
2021-05-06 17:59:44 +0530

    finalupload

--- route53.tf
+++ route53.tf
@@ -2,26 +2,26 @@
 
 
 resource "aws_route53_record" "www-record" {
-    zone_id = 
-    name = "www.kiaasa.shop"
-    type = "A"
-    alias {
-        name = aws_lb.kiaasa-development-load-balancer.dns_name
-        zone_id = aws_lb.kiaasa-development-load-balancer.zone_id
-        evaluate_target_health = false
-    }
+  zone_id = "Z00629922YTL4STT2Q8GH"
+  name    = "www.kiaasa.shop"
+  type    = "A"
+  alias {
+    name                   = aws_lb.kiaasa-development-load-balancer.dns_name
+    zone_id                = aws_lb.kiaasa-development-load-balancer.zone_id
+    evaluate_target_health = false
+  }
 }
 
 # Create Domain Record for CDN
 
 
 resource "aws_route53_record" "cdn-record" {
-    zone_id = 
-    name = "cdndev.kiaasa.shop"
-    type = "A"
-    alias {
-        name = aws_cloudfront_distribution.kiaasa-development-distribution.domain_name
-        zone_id = aws_cloudfront_distribution.kiaasa-development-distribution.hosted_zone_id
-        evaluate_target_health = false
-    }
+  zone_id = "Z00629922YTL4STT2Q8GH"
+  name    = "cdndev.kiaasa.shop"
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.kiaasa-development-distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.kiaasa-development-distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
 }
